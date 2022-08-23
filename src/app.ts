f16c8bdd5204882e90ca02a75e014d5bfbea5913
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-08-24 01:45:25 +0900

    과제까지 성공

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import {getApi} from '@/Common/getApi'
-import { getCourse } from './Common/getCourse';
+import { getCourse } from '@/Common/getBack/getCourse';
+import { getAssignment } from './Common/getBack/getAssignment';
 
 getApi().then(async (res) => {
-	
+	const courses = await new getCourse(res).run();
+	const assignment = await new getAssignment(res).run(courses.activities[0].course.id);
+	console.log(assignment);
 })
